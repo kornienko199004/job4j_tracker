803df629a6e38774be0b2b69d5bb281a646bff80
Kornienko Aleksey <kornienko199004@gmail.com>
2021-09-10 21:39:49 +0300

    1.8. Состояние объекта [#167388]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,27 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public void exchange(Battery another) {
+        this.load += another.load;
+        another.load = 0;
+    }
+
+    public static void main(String[] args) {
+        Battery firstBattery = new Battery(20);
+        Battery secondBattery = new Battery(10);
+
+        System.out.println("First battery load: " + firstBattery.load);
+        System.out.println("Second battery load: " + secondBattery.load);
+
+        firstBattery.exchange(secondBattery);
+
+        System.out.println("First battery load: " + firstBattery.load);
+        System.out.println("Second battery load: " + secondBattery.load);
+    }
+}
