cf1abf0b2d152ef5f7b61f76d7504bf42d6bc35b
Kornienko Aleksey <kornienko199004@gmail.com>
2021-11-29 22:27:05 +0300

    3. Интерфейс Input [#181092 #211111]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -1,18 +1,26 @@
 package ru.job4j.poly;
 
 public class Bus implements Transport {
+    private int passengersCount;
+    private double price;
+    private boolean stop = true;
+
+    public Bus(double price) {
+        this.price = price;
+    }
+
     @Override
     public void ride() {
-
+        this.stop = false;
     }
 
     @Override
     public void passengers(int number) {
-
+        this.passengersCount = number;
     }
 
     @Override
     public double refuel(double count) {
-        return 0;
+        return count * this.price;
     }
 }
