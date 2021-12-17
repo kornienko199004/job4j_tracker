2a13b99a07f17265a9ec68bd4618856d2343db3f
Kornienko Aleksey <kornienko199004@gmail.com>
2021-12-17 23:02:19 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,21 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException{
+        for (int i = 0; i < value.length; i += 1) {
+            if (value[i].equals(key)) {
+                return i;
+            }
+        }
+        throw new ElementNotFoundException("Element not found");
+    }
+
+    public static void main(String[] args) {
+        try {
+            String[] value = { "1", "2" };
+            indexOf(value, "0");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
